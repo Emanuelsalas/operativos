e127e73f429aebb0e4c2c4ffeb0bd09b5e863255
Emanuel_Salas <emmasalas1@hotmail.com>
2023-06-13 11:28:05 -0600

    Acomodo

--- Proyecto/app1.py
+++ Proyecto/app1.py
@@ -15,7 +15,6 @@ classifier = model_from_json(loaded_model_json)
 
 # load weights into new model
 classifier.load_weights("emotion_model1.h5")
-
 #load face
 try:
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
